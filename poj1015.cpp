25fc0ca4f9488d2a45d31a639fab0958035ceb79
yuanxiaopeng <fishballian@qq.com>
2017-11-23 16:56:24 +0800

    dd

--- poj1015.cpp
+++ poj1015.cpp
@@ -2,141 +2,120 @@
 #include<cmath>
 #include<cstdlib>
 using namespace std;
-int a[200][3];
-int diff[200][3];
+int a[200][2];
+int dd[200];
+int dp[20][801][200][3];
 int n, m;
-int r[20];
-int r2[20];
-int D, P, D_P;
-int cut[200][20][2][2];
-
-int cmp1(const void *a, const void *b)
-{
-    return ((int *)a)[0] - ((int *)b)[0];
-}
-
-int cmp2(const void *a, const void *b)
-{
-    int *ap = (int *)a;
-    int *bp = (int *)b;
-    return (bp[0] + bp[1]) - (ap[0] + ap[1]);
-}
 
 int cmp3(const void *a, const void *b)
 {
     return *(int *)a - *(int *)b;
 }
 
-void gen_cut()
+void gen_dp()
 {
-    int i, j, k, c, s, t;
-    qsort(diff, n, sizeof(diff[0]), &cmp1);
-    for(i = 0; i < n; i++)
-    {
-        for(j = 1; j <= m && j + i <= n; j++)
-        {
-            s = c = t = 0;
-            k = 0;
-            while(c < j)
-            {
-                if(diff[k][2] >= i)
-                {
-                    s+=diff[k][0];
-                    t+=diff[k][1];
-                    c++;
-                }
-                k++;
-            }
-            cut[i][j][0][0] = s;
-            cut[i][j][0][1] = t;
+    int i, j, k, u, diff, add, mdiff, mdiff2, madd, s, t, near = 0;
 
-            s = c = t = 0;
-            k = n - 1;
-            while(c < j)
+    for(i = 0; i < m; i++)
+        for(j = -400; j <= 400; j++)
+            for(k = 0; k < n; k++)
+                dp[i][j + 400][k][0] = 400;
+    int min_max[20][2] = {0};
+    qsort(dd, n, sizeof(dd[0]), &cmp3);
+    for(i = 1; i < m; i++)
+    {
+        min_max[i][0] = min_max[i - 1][0] + dd[i - 1];
+        min_max[i][1] = min_max[i - 1][1] + dd[n - i];
+    }
+    for(i = m - 1; i >= 0; i--)
+    {
+        mdiff2 = 400;
+        for(j = min_max[i][0]; j <= min_max[i][1]; j++)
+        {
+            if(abs(j) > mdiff2 + 20)
             {
-                if(diff[k][2] >= i)
-                {
-                    s+=diff[k][0];
-                    t+=diff[k][1];
-                    c++;
-                }
-                k--;
+                continue;
             }
-            cut[i][j][1][0] = s;
-            cut[i][j][1][1] = t;
+            for(k = i; k <= n - (m - i); k++)
+            {
+                mdiff = 400;
+                madd = 0;
+                s = i;
+                for(u = k; u <= n - (m - i); u++)
+                {
+                    if(i < m - 1)
+                    {
+                        t = j + a[u][0] - a[u][1];
+                        diff = dp[i + 1][t + 400][u + 1][0];
+                        add = dp[i + 1][t + 400][u + 1][1] + a[u][0] + a[u][1];
+                    }
+                    else
+                    {
+                        diff = j + a[u][0] - a[u][1];
+                        add = a[u][0] + a[u][1];
+                    }
+                    if(abs(diff) < abs(mdiff) || (abs(diff) == abs(mdiff) && add > madd))
+                    {
+                        mdiff = diff;
+                        madd = add;
+                        s = u;
+                    }
+                }
+                dp[i][j + 400][k][0] = mdiff;
+                dp[i][j + 400][k][1] = madd;
+                dp[i][j + 400][k][2] = s;
+                mdiff2 = min(abs(mdiff), mdiff2);
+            }
         }
     }
-
-    //for(i = 0; i < n; i++)
-    //{
-    //    for(j = 1; j <= m && j + i <= n; j++)
-    //        printf("%d,%d,%d,%d ", cut[i][j][0][0], cut[i][j][0][1], cut[i][j][1][0], cut[i][j][1][1]);
-    //    printf("\n");
-    //}
-}
-
-void dfs(int d, int u, int td, int tp)
-{
-    //if(D_P == 0)
-    //    return;
-    int i, delta, t1, t2;
-    t1 = td - tp + cut[u][m - d][0][0];
-    t2 = td - tp + cut[u][m - d][1][0];
-    if(t1 >= 0 && t2 >= 0 && (t1 > D_P || (t1 == D_P && td + tp + cut[u][m - d][0][1] <= D + P)))
-        return;
-    if(t1 <= 0 && t2 <= 0 && (abs(t2) > D_P || (abs(t2) == D_P && td + tp + cut[u][m - d][1][1] <= D + P)))
-        return;
-    if(d >= m)
-    {
-        delta = abs(td - tp) - abs(D - P);
-        if(delta < 0 || (delta == 0 && td + tp > D + P))
-        {
-            for(i = 0; i < m; i++)
-                r2[i] = r[i];
-            D = td;
-            P = tp;
-            D_P = abs(D - P);
-        }
-        return;
-    }
-    if(u >= n)
-        return;
-    r[d] = a[u][2];
-    dfs(d + 1, u + 1, td + a[u][0], tp + a[u][1]);
-    dfs(d, u + 1, td, tp);
 }
 
 int main()
 {
-    int i, c = 0;
+    int i, s, c = 0;
+    int mdiff, madd, D, P;
+    int r[20];
 #ifdef _POJ
     freopen("poj1015.txt","r",stdin);
 #endif
+
     while(cin >> n && n > 0)
     {
         cin >> m;
         for(i = 0; i < n; i++)
         {
             cin >> a[i][0] >> a[i][1];
-            a[i][2] = i;
+            dd[i] = a[i][0] - a[i][1];
         }
-        //qsort(a, n, sizeof(a[0]), &cmp2);
-        for(i = 0; i < n; i++)
+        gen_dp();
+        mdiff = 400;
+        madd = 0;
+        for(i = 0; i < n - m + 1; i++)
         {
-            diff[i][0] = a[i][0] - a[i][1];
-            diff[i][1] = a[i][0] + a[i][1];
-            diff[i][2] = i;
+            if(abs(dp[0][400][i][0]) < mdiff || (abs(dp[0][400][i][0]) == mdiff && dp[0][400][i][1] > madd))
+            {
+                s = dp[0][400][i][2];
+                mdiff = abs(dp[0][400][i][0]);
+                madd = dp[0][400][i][1];
+            }
         }
-        D = 20 * 20;
-        P = 0;
-        D_P = abs(D - P);
-        gen_cut();
-        dfs(0, 0, 0, 0);
+        r[0] = s;
+        D = P = mdiff = 0;
+        for(i = 1; i < m; i++)
+        {
+            mdiff += a[r[i - 1]][0] - a[r[i - 1]][1];
+            r[i] = dp[i][mdiff + 400][r[i - 1] + 1][2];
+        }
+        for(i = 0; i < m; i++)
+        {
+            D += a[r[i]][0];
+            P += a[r[i]][1];
+        }
         cout << "Jury #" << ++c << endl;
         cout << "Best jury has value " << D << " for prosecution and value " << P <<" for defence:"<< endl;
-        qsort(r2, m, sizeof(r2[0]), &cmp3);
+        //qsort(r, m, sizeof(r[0]), &cmp3);
         for(i = 0; i < m; i++)
-            cout << " " << r2[i] + 1;
+            cout << " " << r[i] + 1;
         cout << endl << endl;
     }
     return 0;
