49b0a669caef46ded7db68b6f33ffacd006c06e6
yuanxiaopeng <fishballian@qq.com>
2017-12-06 15:54:36 +0800

    d

--- poj1050.c
+++ poj1050.c
@@ -2,6 +2,7 @@
 #include<limits.h>
 #include<math.h>
 char matrix[100][100];
+int b[100][100][100];
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
@@ -11,7 +12,7 @@ int main()
 #ifdef _POJ
     freopen("poj1050.txt", "r", stdin);
 #endif
-    while(scanf("%d", &N) != EOF)
+    while(scanf("%d", &N) != EOF && N != 0)
     {
         Max = INT_MIN;
         for(int i = 0; i < N; i++)
@@ -19,15 +20,31 @@ int main()
                 scanf("%d", &matrix[i][j]);
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
+            {
+                s = 0;
+                for(int k = i; k < N; k++)
+                {
+                    s += matrix[k][j];
+                    b[i][j][k - i] = s;
+                    //printf("%d %d %d %d\n", i, j, k - i, s);
+                }
+            }
+
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
                 for(int k = 0; k + i < N; k++)
-                    for(int l = 0; l + j < N; l++)
+                {
+                    s = 0;
+                    for(int m = 0; m <= j; m++)
+                        s += b[k][m][i];
+                    Max = max(s, Max);
+                    for(int l = 1; l + j < N; l++)
                     {
-                        s = 0;
-                        for(int m = k; m <= k + i; m++)
-                            for(int n = l; n <= l + j; n++)
-                                s += matrix[m][n];
+                        s -= b[k][l - 1][i];
+                        s += b[k][l + j][i];
                         Max = max(s, Max);
                     }
+                }
         printf("%d\n", Max);
     }
     return 0;
