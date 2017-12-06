3e9651d64250194e67eba0b43d19c9617b59e700
yuanxiaopeng <fishballian@qq.com>
2017-12-06 14:12:25 +0800

    ss

--- poj1050.cpp
+++ poj1050.cpp
@@ -1,21 +1,34 @@
 #include<stdio.h>
+#include<limits.h>
+#include<math.h>
 char matrix[100][100];
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 int main()
 {
-    int N;
+    int N, Max, s;
 #ifdef _POJ
     freopen("poj1050.txt", "r", stdin);
 #endif
     while(scanf("%d", &N) != EOF)
     {
+        Max = INT_MIN;
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
                 scanf("%d", &matrix[i][j]);
-
         for(int i = 0; i < N; i++)
             for(int j = 0; j < N; j++)
-                printf("%d\n", matrix[i][j]);
+                for(int k = 0; k + i < N; k++)
+                    for(int l = 0; l + j < N; l++)
+                    {
+                        s = 0;
+                        for(int m = k; m <= k + i; m++)
+                            for(int n = l; n <= l + j; n++)
+                                s += matrix[m][n];
+                        Max = max(s, Max);
+                    }
+        printf("%d\n", Max);
     }
     return 0;
 }
