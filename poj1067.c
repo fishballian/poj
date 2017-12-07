b81ce88a4bab8f3b3128c6718c8f5ed8940fe565
yuanxiaopeng <fishballian@qq.com>
2017-12-07 19:39:52 +0800

    dd

--- poj1067.c
+++ poj1067.c
@@ -10,23 +10,23 @@ int max(int a, int b)
 {
     return a > b ? a : b;
 }
+#define u (sqrt(5.0)+1)/2
 
 int main()
 {
     int a, b, Min, Max;
+    //double golden = (sqrt(5) - 1) / 2;
 #ifdef _POJ
     freopen("poj1067.txt", "r", stdin);
 #endif
     while(scanf("%d%d", &a, &b) != EOF)
     {
-        if(a == 0 || b == 0 || a == b)
-            printf("1\n");
+        Min = min(a, b);
+        Max = max(a, b);
+        if(floor((Max - Min) * u) == Min)
+            printf("0\n");
         else
-        {
-            Min = min(a, b);
-            Max = max(a, b);
-        }
-                
+            printf("1\n");
     }
     return 0;
 }
