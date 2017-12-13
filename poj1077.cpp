1f97ea23e8af36cdcd25ce7d40c921b29bba1dd6
yuanxiaopeng <fishballian@qq.com>
2017-12-13 17:04:52 +0800

    dd

--- poj1077.cpp
+++ poj1077.cpp
@@ -15,7 +15,7 @@ bool do_move(string &s, char d);
 
 char revert(char dir)
 {
-    char mv;
+    char mv = 'l';
     switch(dir)
     {
         case 'u':
@@ -26,9 +26,6 @@ char revert(char dir)
             break;
         case 'l':
             mv = 'r';
-            break;
-        case 'r':
-            mv = 'l';
             break;
     }
     return mv;
@@ -47,7 +44,7 @@ class node
             if(do_move(s, dir))
             {
                 len++;
-                mv = revert(dir);
+                mv = dir;
                 f = len + calc(s);
             }
             else
@@ -84,15 +81,25 @@ bool astar_search(string str)
         {
             finish = true;
             s2 = n.s;
-            while((t = closeset.find(s2)->second) != 's')
+            while(true)
             {
-                st.push(t);
-                do_move(s2, t);
+                if(closeset.find(s2) != closeset.end())
+                {
+                    t = closeset.find(s2)->second;
+                    if(t == 's')
+                        break;
+                    st.push(t);
+                    do_move(s2, revert(t));
+                }
+                else
+                {
+                    exit(-1);
+                }
             }
             count = 0;
             while(!st.empty())
             {
-                res[count++] = revert(st.top());
+                res[count++] = st.top();
                 st.pop();
             }
             res[count++] = '\0';
@@ -180,8 +187,16 @@ int main()
     assert(calc("12345678x") == 0);
     assert(calc("1234567x8") == 2);
     assert(calc("12345x786") == 2);
+    char xx[40] = "dlurullddrurdllurdr";
+    ori = "23415x768";
+    for(i = 0; xx[i] != '\0'; i++)
+    {
+        assert(do_move(ori, xx[i]));
+    }
+    assert(ori == "12345678x");
 #endif
-    while(scanf("%s", s) != EOF)
+
+    while(scanf("%s", s) != EOF && s[0] != '0')
     {
         ori[0] = s[0];
         for(i = 1; i < 9; i++)
@@ -189,35 +204,35 @@ int main()
             scanf("%s", s);
             ori[i] = s[0];
         }
-
+        //cout << ori << "==";
         sum = 0;
         for(i = 0; i < 8; i++)
             for(j = i + 1; j < 9; j++)
                 if(ori[j] != 'x' && ori[i] != 'x' && ori[j] < ori[i])
                     sum++;
-        cout << ori << "=";
         if(sum % 2 == 1)
         {
-            assert(astar_search(ori) == false);
-            printf("unsolvable\n");
+            //assert(astar_search(ori) == false);
+            cout << "unsolvable" << endl;
+        }
+        else if(ori == "12345678x")
+        {
+            printf("lr\n");
         }
         else if(astar_search(ori))
         {
-            for(i = 0; res[i] != '\0'; i++)
-            {
-                do_move(ori, res[i]);
-            }
-            assert(ori == "12345678x");
-            printf("%s\n", res);
+            //for(i = 0; res[i] != '\0'; i++)
+            //{
+            //    assert(do_move(ori, res[i]));
+            //}
+            //assert(ori == "12345678x");
+            cout << res << endl;
         }
         else
         {
-            printf("unknown error, ori=");
-            cout << ori << endl;
+            cout << "unsolvable" << endl;
         }
-
     }
-
     return 0;
 }
 
