70c98f140c5a7065c1db0bf8f285a548569cdd35
yuanxiaopeng <fishballian@qq.com>
2017-12-15 11:05:55 +0800

    ddd

--- sort.java
+++ sort.java
@@ -1,6 +1,19 @@
-public class HelloWorld {
-    public static void main()
+import java.util.Arrays;
+import java.util.Comparator;
+class CmpString implements Comparator{
+    public int compare(Object a, Object b)
     {
-        System.out.println("Hello World!");
+        String as = (String)a;
+        String bs = (String)b;
+        return as.length() - bs.length(); 
+    }
+}
+public class sort{
+    public static void main(String args[])
+    {
+        String[] a = {"dfe", "aaa", "defg", "dd"};
+        Arrays.sort(a, new CmpString());
+        for(int i = 0; i < 4; i++)
+            System.out.println(a[i]);
     }
 }
