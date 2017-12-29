dfd39724b456ce9a6f81ed4c94054652d4fafc91
yuanxiaopeng <fishballian@qq.com>
2017-12-29 18:35:19 +0800

    dd

--- poj1026.cpp
+++ poj1026.cpp
@@ -4,9 +4,21 @@ using namespace std;
 
 int main()
 {
+    int n, k, a[200];
+    char s[200];
 #ifdef _POJ
-    freopen("poj1000.txt", "r", stdin);
+    freopen("poj1026.txt", "r", stdin);
 #endif
+    while(cin >> n && n > 0)
+    {
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+        while(cin >> k && k > 0)
+        {
+            scanf("%[^\n]", s);
+            cout << s << endl;
+        }
+    }
     return 0;
 }
 
